973851174d7af65c87618850fcbae606ccbbb65b
sh-misaki <s.misaki.0000@gmail.com>
2019-12-18 23:34:46 +0900

    works controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'home/top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'works/index'
+  get 'works/create'
+  resource :home, only: [:index]
 end
