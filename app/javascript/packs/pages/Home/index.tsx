4b77d84ec9428d208d80f2b8a06f11ee3f00b468
sh-misaki <s.misaki.0000@gmail.com>
2019-12-18 22:50:01 +0900

    リストページ スタイル

--- app/javascript/packs/pages/Home/index.tsx
+++ app/javascript/packs/pages/Home/index.tsx
@@ -1,8 +1,9 @@
-import React, { FunctionComponent } from 'react'
+import * as React from 'react'
 import injectSheet, { WithStyles } from 'react-jss';
 import {
   Typography
 } from "@material-ui/core";
+import axios from "axios";
 
 import MainLayout from "../../layouts/Main";
 import styles, { IStyleProps } from "./styles";
@@ -13,15 +14,69 @@ interface IOwnProps {
 
 type IProps = IOwnProps & IStyleProps;
 
-const Hello: FunctionComponent<IProps> = ({
-  name,
-  classes
-}) => (
-  <MainLayout>
-    <Typography classes={{ root: classes.hello }}>
-      Hello {name}!
-    </Typography>
-  </MainLayout>
-)
+interface IState {
+  objects: any[];
+}
+
+class Hello extends React.Component<IProps, IState> {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      objects: [],
+    };
+  }
+
+  public async componentDidMount() {
+    const { data: { objectIDs } } = await axios.get('https://collectionapi.metmuseum.org/public/collection/v1/search', {
+      params: {
+        departmentId: 21,
+        q: "a",
+      }
+    })
+
+    const datas = await Promise.all(objectIDs.slice(0, 30).map(async objectID => {
+      const { data } = await axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`);
+      return data;
+    }))
+
+    this.setState({
+      objects: datas,
+    });
+  }
+
+  public render() {
+    const {
+      name,
+      classes,
+    } = this.props;
+    const { objects } = this.state;
+
+    console.log(objects)
+
+    return (
+      <MainLayout>
+        <div className={classes.objectWrapper}>
+          {objects.map(object => (
+            <div className={classes.object}>
+              <img
+                className={classes.objectImage}
+                src={object.primaryImageSmall}
+              />
+              <div className={classes.objectOverview}>
+                <Typography classes={{root: classes.objectTitle}}>
+                  { object.title }
+                </Typography>
+                <Typography classes={{root: classes.objectArtist}}>
+                  { object.artistDisplayName } ({ object.artistBeginDate } ~ { object.artistEndDate })
+                </Typography>
+              </div>
+            </div>
+          ))}
+        </div>
+      </MainLayout>
+    )
+  }
+}
 
 export default injectSheet(styles)(Hello);
--- app/javascript/packs/pages/Home/styles.ts
+++ app/javascript/packs/pages/Home/styles.ts
@@ -1,9 +1,54 @@
 import { WithStyles } from 'react-jss';
 
 const styles = {
-  hello: {
-    color: 'green',
-  }
+  objectWrapper: {
+    display: 'flex',
+    flexFlow: 'column wrap',
+    alignContent: 'space-between',
+    height: '5000px',
+    '&:before': {
+      content: '""',
+      flexBasis: '100%',
+      width: '0',
+      order: '2',
+    },
+    '&:after': {
+      content: '""',
+      flexBasis: '100%',
+      width: '0',
+      order: '2',
+    }
+  },
+  object: {
+    width: '31%',
+    padding: '16px',
+    boxShadow: '8px 8px 16px rgba(0,0,0,0.25)',
+    borderRadius: '4px',
+    marginBottom: '48px',
+    '&:nth-child(3n+1)': {
+      order: 1,
+    },
+    '&:nth-child(3n+2)': {
+      order: 2,
+    },
+    '&:nth-child(3n)': {
+      order: 3,
+    },
+  },
+  objectImage: {
+    width: '100%',
+  },
+  objectOverview: {
+    marginTop: '16px',
+  },
+  objectTitle: {
+    fontSize: '15px',
+  },
+  objectArtist: {
+    fontSize: '13px',
+    textAlign: 'right',
+    marginTop: '8px',
+  },
 }
 
 export type IStyleProps = WithStyles<Record<keyof typeof styles, string>>;
