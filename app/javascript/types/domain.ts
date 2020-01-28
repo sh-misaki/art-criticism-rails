f3dc80d60a04990c22298e0b28880b671bdb8a0a
sh-misaki <s.misaki.0000@gmail.com>
2020-01-28 23:25:20 +0900

    interface作成

---
+++ app/javascript/types/domain.ts
@@ -0,0 +1,27 @@
+
+
+export namespace Domains {
+  export interface Work {
+    id: number;
+    name: string;
+    primaryImage: string | null;
+    primaryImageSmall: string | null;
+    department: string | null;
+    classification: string | null;
+    year: string | null;
+    artist_id: string | null;
+    artist_name: string | null;
+    source: "metropolitan" | null;
+    source_id: string | null;
+    created_at: Date;
+    updated_at: Date;
+  }
+  export interface Artist {
+    id: number;
+    name: string;
+    nationality: string | null;
+    beginYear: number | null;
+    endYear: number | null;
+    role: string | null;
+  }
+}
